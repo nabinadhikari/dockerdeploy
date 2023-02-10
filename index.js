c1df3f884fae41592a7f040ed8fd6a7837f1019d
Nabin Adhikari <mr.nabinadhikari@gmail.com>
2023-02-11 10:56:59 +1100

    lets see if connects to mongodb

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const uuid = require("uuid");
+
+const mongoDB =
+  "mongodb://root:rootpassword@localhost:27017/?authMechanism=DEFAULT";
+
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
+const TodoModelSchema = new mongoose.Schema({
+  todo: String,
+  createdAt: Date,
+});
+
+const TodoModel = mongoose.model("TodoModel", TodoModelSchema);
+
+mongoose
+  .connect(mongoDB)
+  .then(() => console.log("MongoDb connected"))
+  .catch((e) => console.log("Error connecting to mongodb", e));
+
+app.get("/", async (req, res) => {
+  const todo = await new TodoModel({
+    todo: uuid.v4(),
+    createdAt: new Date(),
+  }).save();
+  console.log(todo);
+  const todos = await TodoModel.find();
   return res.status(200).json({
     status: 200,
     message: "Hit!",
+    todo,
+    todos,
   });
 });
 
